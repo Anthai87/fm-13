6563379fe657da39ec649f9f74d283000e672986
HLLissau <hneltrup@hotmail.com>
2024-01-10 11:22:40 +0100

    green tests

--- EndToEndTests/src/test/java/dtu/redGreenRefactor/PaymentServiceSteps.java
+++ EndToEndTests/src/test/java/dtu/redGreenRefactor/PaymentServiceSteps.java
@@ -6,9 +6,11 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.math.BigDecimal;
 import java.util.List;
 
+import dtu.fm13.customer.CustomerService;
 import dtu.fm13.customer.PaymentService;
 import dtu.fm13.customer.model.Customer;
 import dtu.fm13.customer.model.Payment;
+import dtu.ws.fastmoney.Account;
 import dtu.ws.fastmoney.BankService;
 import dtu.ws.fastmoney.BankServiceException_Exception;
 import dtu.ws.fastmoney.BankServiceService;
@@ -26,6 +28,8 @@ public class PaymentServiceSteps {
 	private List<Payment> payments;
 	private final BankService bankService = new BankServiceService().getBankServicePort();
 	private String returnID;
+	private CustomerService customerService = new CustomerService();
+	private int responseCode;;
 
 	// Harald
 	@Given("a customer with id {string}")
@@ -39,7 +43,7 @@ public class PaymentServiceSteps {
 	@Given("a merchant with id {string}")
 	public void aMerchantWithId(String id) {
 		merchant = new Customer("merchant", "name");
-		
+
 		merchant.setId(id);
 
 	}
@@ -87,14 +91,16 @@ public class PaymentServiceSteps {
 		user.setCprNumber("1234567891");
 		user.setFirstName("Harald");
 		user.setLastName("testLastname");
+		customer = new Customer();
+		customer.setFirstName(user.getFirstName());
+		customer.setCpr(user.getCprNumber());
+		customer.setLastName(user.getLastName());
 
 		try {
 			returnID = bankService.createAccountWithBalance(user, new BigDecimal(int1));
 			System.out.println(returnID);
 			customer.setId(returnID);
-			customer.setFirstName(user.getFirstName());
-			customer.setCpr(user.getCprNumber());
-			customer.setLastName(user.getLastName());
+
 		} catch (BankServiceException_Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -104,32 +110,60 @@ public class PaymentServiceSteps {
 
 	@Given("that the customer is registered with DTU Pay")
 	public void thatTheCustomerIsRegisteredWithDTUPay() {
-		// Write code here that turns the phrase above into concrete actions
-		throw new io.cucumber.java.PendingException();
+		responseCode = customerService.add(customer);
+		assertEquals(responseCode, 204);
+
 	}
 
 	@Given("a merchant with a bank account with balance {int}")
 	public void aMerchantWithABankAccountWithBalance(Integer int1) {
-		// Write code here that turns the phrase above into concrete actions
-		throw new io.cucumber.java.PendingException();
+		User user = new User();
+		user.setCprNumber("1098765432");
+		user.setFirstName("Mechant");
+		user.setLastName("testmerchant");
+		merchant = new Customer();
+		merchant.setFirstName(user.getFirstName());
+		merchant.setCpr(user.getCprNumber());
+		merchant.setLastName(user.getLastName());
+		try {
+			returnID = bankService.createAccountWithBalance(user, new BigDecimal(int1));
+			System.out.println(returnID + "here");
+			merchant.setId(returnID);
+			
+		} catch (BankServiceException_Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	@Given("that the merchant is registered with DTU Pay")
 	public void thatTheMerchantIsRegisteredWithDTUPay() {
-		// Write code here that turns the phrase above into concrete actions
-		throw new io.cucumber.java.PendingException();
+		responseCode = customerService.add(merchant);
+		assertEquals(responseCode, 204);
 	}
 
 	@Then("the balance of the customer at the bank is {int} kr")
 	public void theBalanceOfTheCustomerAtTheBankIsKr(Integer int1) {
-		// Write code here that turns the phrase above into concrete actions
-		throw new io.cucumber.java.PendingException();
+		BigDecimal balance= new BigDecimal(-1);
+		try {
+			balance = bankService.getAccount(customer.getId()).getBalance();
+		} catch (BankServiceException_Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		assertEquals(balance,int1);
 	}
 
 	@Then("the balance of the merchant at the bank is {int} kr")
 	public void theBalanceOfTheMerchantAtTheBankIsKr(Integer int1) {
-		// Write code here that turns the phrase above into concrete actions
-		throw new io.cucumber.java.PendingException();
+		BigDecimal balance= new BigDecimal(-1);
+		try {
+			balance = bankService.getAccount(merchant.getId()).getBalance();
+		} catch (BankServiceException_Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		assertEquals(balance,int1);
 	}
 
 }
