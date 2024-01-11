3571c563c06d9dd5e54b48aed42280679265af31
anthai87 <s141479@student.dtu.dk>
2024-01-11 16:43:46 +0100

    added lombok to pomXML

--- EndToEndTests/src/main/java/dtu/fm13/customer/model/Payment.java
+++ EndToEndTests/src/main/java/dtu/fm13/customer/model/Payment.java
@@ -1,13 +1,21 @@
 package dtu.fm13.customer.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.Objects;
 import java.util.UUID;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Payment {
-	private UUID payerId;
-	private UUID recieverId;
-	private int amount;
-	public Payment() {
+    private UUID payerId;
+    private UUID recieverId;
+    private int amount;
+
+	/*public Payment() {
 		
 	}
 	public Payment(UUID payerId,UUID recieverId,int amount) {
@@ -38,20 +46,21 @@ public class Payment {
 
 	public void setAmount(int amount) {
 		this.amount = amount;
-	}
-	
-	@Override
-	public boolean equals(Object obj) {
-	    if (this == obj) return true;
-	    if (obj == null || getClass() != obj.getClass()) return false;
-	    Payment payment = (Payment) obj;
-	    return Objects.equals(amount,payment.amount) &&
-	    		Objects.equals(payerId,payment.payerId) &&
-	    		Objects.equals(recieverId,payment.recieverId);
-	}
-	@Override
+	}*/
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Payment payment = (Payment) obj;
+        return Objects.equals(amount, payment.amount) &&
+                Objects.equals(payerId, payment.payerId) &&
+                Objects.equals(recieverId, payment.recieverId);
+    }
+
+    @Override
     public int hashCode() {
-        return Objects.hash(payerId, recieverId,amount);
+        return Objects.hash(payerId, recieverId, amount);
     }
 }
 
