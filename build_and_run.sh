b95bf62df99f0fcb929f0af2478be8620c12cb78
HLLissau <hneltrup@hotmail.com>
2024-01-16 14:52:51 +0100

    Added swagger

--- build_and_run.sh
+++ build_and_run.sh
@@ -10,6 +10,7 @@ cd PaymentMicroService
 ./build.sh 
 cd ..
 
+docker compose -f ./Swagger/swagger.yml up --force-recreate -d
 docker-compose -f docker-compose.yml up -d --build --force-recreate 
 
 cd EndToEndTests
